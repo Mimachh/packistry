8db14d7629927274ca1d4a8c3492398afc23860e
Karl <karl.mullr@gmail.com>
2025-05-10 08:51:06 +0200

    feat: add backup

--- app/Bootstrappers/ScheduleBootstrapper.php
+++ app/Bootstrappers/ScheduleBootstrapper.php
@@ -7,5 +7,9 @@ use Illuminate\Console\Scheduling\Schedule;
 class ScheduleBootstrapper
 {
     // php artisan schedule:list
-    public function __invoke(Schedule $schedule): void {}
+    public function __invoke(Schedule $schedule): void
+    {
+        $schedule->command('backup:clean')->daily()->at('01:00');
+        $schedule->command('backup:run')->daily()->at('01:30');
+    }
 }
